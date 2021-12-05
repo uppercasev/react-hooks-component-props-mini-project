68367d48cd4fe8fea9113df0b80fd4a5898abce8
Vi Tran <contact.vitran@gmail.com>
2021-12-05 22:01:20 +1100

    Main deliverables complete and tests passed

--- src/components/App.js
+++ src/components/App.js
@@ -9,11 +9,9 @@ console.log(blogData);
 function App() {
   return (
     <div className="App">
-      {/* You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck! */}
       <Header name={blogData.name} />
       <About about={blogData.about} image={blogData.image} />
-      <ArticleList list={blogData.posts} />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Article from "./Article";
 
-function ArticleList({ list }) {
-  return (
+function ArticleList({ posts = [] }) {
+    return (
     <div id="article_list">
       <main>
-        {list.map(function({ id, title, preview, date}) {
+        {posts.map(function({ id, title, preview, date }) {
             return <Article key={id} title={title} preview={preview} date={date} />;
         })}
       </main>
